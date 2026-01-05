7ebe5cde8efed5859eeeb337911dad4edc2be368
Dam Ogundeji <dam.ogundeji@gmail.com>
2026-01-05 21:50:06 +1100

    Add dynamic hello endpoint

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, world. I built this."}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello {name}, this app is alive on the internet."}
